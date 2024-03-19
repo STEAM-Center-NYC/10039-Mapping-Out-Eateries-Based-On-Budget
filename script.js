2fed4f7e96342788bf03d0eb473226106f6fcfb0
kasloww <kdavidson@brooklynsteamcenter.org>
2024-03-19 10:36:23 -0400

    not done

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ const toggleNav = () => {
   mobileNav.classList.toggle("hamburger-active");
 };
 mobileNav.addEventListener("click", () => toggleNav());
+
+const f = document.getElementById('form');
+      const q = document.getElementById('query');
+      
+    
+
+      function submitted(event) {
+        event.preventDefault();
+        const url = google + '+' + q.value;
+        const win = window.open(url, '_blank');
+        win.focus();
+      }
+
+      f.addEventListener('submit', submitted);
