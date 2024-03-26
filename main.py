1d0bbde0001fce95a0b28d586588512b364428eb
kasloww <kdavidson@brooklynsteamcenter.org>
2024-03-26 10:55:05 -0400

    fix sql code and syntax in general

--- main.py
+++ main.py
@@ -12,6 +12,21 @@ settings = Dynaconf(
     settings_file=['settings.toml']
 )
 
+def get_id(self):
+    return str(self.id)
+
+def get_db():
+    '''Opens a new database connection per request.'''        
+    if not hasattr(g, 'db'):
+        g.db = connect_db()
+    return g.db    
+
+@app.teardown_appcontext
+def close_db(error):
+    '''Closes the database connection at the end of request.'''    
+    if hasattr(g, 'db'):
+        g.db.close() 
+
 def connect_db():
     return pymysql.connect(
         host="10.100.33.60",
@@ -21,10 +36,19 @@ def connect_db():
         cursorclass=pymysql.cursors.DictCursor,
         autocommit=True
     )
-@app.route('/home')
+@app.route('/')
 def index():
-    return render_template('index.html.jinja')
+    cursor = get_db().cursor()
+    if request.method == 'POST':
+        cursor.execute("SELECT * FROM `Name`")
+        rests = cursor.fetchall()
+        get_db().commit()
+    return render_template('index.html.jinja', rests=rests)
+
 
 @app.route('/aboutus')
 def about():
     return render_template('Aboutus.html.jinja')
+
+if __name__ == '__main__':
+    app.run(debug=True)
